3293c2d55a569a3246bca192a92c65a77117bc9b
Rytar33 <maks.Khromakov@mail.ru>
2024-01-09 13:31:30 +0200

    feat Добавил Storage, вычисление: мин., макс., сред. возраста и фильтрацию

---
+++ Application/Services/Storage/EmployeeStorage.cs
@@ -0,0 +1,24 @@
+﻿using Models;
+using System.Collections;
+
+namespace Services.Storage
+{
+    public class EmployeeStorage : IEnumerable<Employee>
+    {
+        private Employee[] _employees;
+        public EmployeeStorage(Employee[] employees) => _employees = employees;
+        public EmployeeStorage() => _employees = new Employee[0];
+        public Employee this[int index] => _employees[index];
+        public void Add(Employee employee)
+        {
+            Array.Resize(ref _employees, _employees.Length + 1);
+            _employees[^1] = employee;
+        }
+        public void Insert(int id, Employee employee) 
+            => _employees[id] = employee;
+        public IEnumerator<Employee> GetEnumerator() 
+            => ((IEnumerable<Employee>)_employees).GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
